c45142cd23f8f592639ff9a99b92311a29bb1f5a
haarrublar <harrubla.96@gmail.com>
2024-03-24 14:01:18 -0400

    removing unnecessary .py files

--- website/settings.py
+++ website/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'website.middleware.HtmlRedirectMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
