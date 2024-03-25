06150efb58d57c6873ac3895b232f88a49f42e06
haarrublar <harrubla.96@gmail.com>
2024-03-25 17:09:57 -0400

    completing Post component variables - setting meta class for ordering and indexing Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# # by default/easiest registration models
+# admin.site.register(Post)
+
+# custom registering models 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','author','created','status']
+    list_filter = ['author','status']
--- blog/models.py
+++ blog/models.py
@@ -7,15 +7,41 @@ Each class creates a DB.
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        """
+        Create a status per blog. Two status allowed: draft and published depicting incomplete /in progress and completed respectively. 
+        """
+        draft = 'df','draft'
+        published = 'pb','published'
+
     title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    # the slug term is for URL creation
     slug = models.SlugField(max_length=250)
-    body = models.TextField()
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='blog_posts'
     )
+    body = models.TextField()
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.draft
+    )
+
+
+    class Meta:
+        """
+        Meta-class of Post class setting an ordering of the post based on the data of creation and setting indexes to the post for enhancing searching (higher search speed, lower read speed)
+        """
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
 
     def __str__(self):
         return self.title
