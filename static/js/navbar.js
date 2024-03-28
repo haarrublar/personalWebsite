2abc3721c0b8b95ed95066c0f4940545abda9e93
haarrublar <harrubla.96@gmail.com>
2024-03-28 01:46:07 -0400

    blog template css and style completed

---
+++ static/js/navbar.js
@@ -0,0 +1,8 @@
+// Get the navbar elements
+const navbarToggler = document.getElementById('navbar-toggler');
+const navbarMenu = document.getElementById('navbar-menu');
+
+// Add event listener for toggling the navbar menu
+navbarToggler.addEventListener('click', () => {
+  navbarMenu.classList.toggle('hidden');
+});
